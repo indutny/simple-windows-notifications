cb70b450436467021f540b858bb4ec0c9a651861
Fedor Indutny <238531+indutny@users.noreply.github.com>
2024-09-13 10:11:59 -0700

    chore: better tests

--- test/api_test.ts
+++ test/api_test.ts
@@ -1,18 +1,22 @@
-import test from 'ava';
+import test, { type ExecutionContext } from 'ava';
 
 import { Notifier, sendDummyKeystroke } from '../index';
 
-test('sends dummy keystroke', async (t) => {
+function platformWrap(t: ExecutionContext, fn: () => void): void {
   if (process.platform === 'win32') {
-    t.notThrows(() => sendDummyKeystroke());
+    t.notThrows(fn);
   } else {
-    t.throws(() => sendDummyKeystroke(), {
+    t.throws(fn, {
       message: 'This library works only on Windows',
     });
   }
+}
+
+test('sends dummy keystroke', async (t) => {
+  platformWrap(t, () => sendDummyKeystroke());
 });
 
-test('shows and removes notification', async (t) => {
+test('shows a notification', async (t) => {
   const notifier = new Notifier('org.indutny.test');
 
   const toastXml = `<toast>
@@ -24,20 +28,44 @@ test('shows and removes notification', async (t) => {
     </visual>
   </toast>`;
 
-  if (process.platform === 'win32') {
-    t.notThrows(() => {
-      notifier.clearAll();
-      notifier.show(toastXml, { tag: 'tag', group: 'group' });
-      notifier.remove({ tag: 'tag', group: 'group' });
+  platformWrap(t, () => {
+    notifier.show(toastXml, { tag: 'tag', group: 'group' });
+  });
+});
+
+test('shows a notification that expires on reboot', async (t) => {
+  const notifier = new Notifier('org.indutny.test');
+
+  const toastXml = `<toast>
+    <visual>
+      <binding template="ToastText02">
+        <text id="1">headlineText</text>
+        <text id="2">bodyText</text>
+      </binding>
+    </visual>
+  </toast>`;
+
+  platformWrap(t, () => {
+    notifier.show(toastXml, {
+      tag: 'tag',
+      group: 'group',
+      expiresOnReboot: true,
     });
-  } else {
-    t.throws(
-      () => {
-        notifier.clearAll();
-        notifier.show(toastXml, { tag: 'tag', group: 'group' });
-        notifier.remove({ tag: 'tag', group: 'group' });
-      },
-      { message: 'This library works only on Windows' },
-    );
-  }
+  });
+});
+
+test('clears a notification', async (t) => {
+  const notifier = new Notifier('org.indutny.test');
+
+  platformWrap(t, () => {
+    notifier.remove({ tag: 'tag', group: 'group' });
+  });
+});
+
+test('clears all notifications', async (t) => {
+  const notifier = new Notifier('org.indutny.test');
+
+  platformWrap(t, () => {
+    notifier.clearAll();
+  });
 });
